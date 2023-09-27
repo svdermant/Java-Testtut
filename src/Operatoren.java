ce64596fd598ec26f13149ee73fead120531ff2d
svdermant <svdermant@gmail.com>
2023-09-27 23:30:54 +0200

    ^ Exclusive or Operator

--- src/Operatoren.java
+++ src/Operatoren.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
 public class Operatoren {
     public static void main(String[] args) {
         //Logische Operatoren sind Grundlage in Java für Professionalen Code
@@ -5,6 +7,20 @@ public class Operatoren {
         // Bedeutet das gleiche wie (Temparatur <= 20)
         // && <- Und Operator Besagt wenn if bedingung1 und if bedingung2 zutrifft führe code aus.
         // || <- Oder Operator Besagt das eine beidingung Mindestens erfüllt sein muss um Code auszuführen.
+        // Letzter Operator wäre das Exclusive or (^)
+        // Bei einem Spiel wird ein Gewinner festgelegt entweder jan oder tim wenn einer von beiden gewonnen hat
+        // Hierbei trifft die bedingung nur zu wenn eine der Angegeben werte ein true wiedergibt
+        // beide werte auf false oder true geben ein false zurück.
+        // Beispiel:
+        boolean jan_gewonnen = true;
+        boolean tim_gewonnen = false;
+
+        //If Statement
+        if(jan_gewonnen ^ tim_gewonnen) {
+            System.out.println("Richtiges Ergebnis");
+        } else {
+            System.out.println("Falsches Ergebniss");
+        }
 
         double temperatur = 5.5; // 45° Sind nicht Angenehm Die folgende If Bedingung liefert also ein
         // Falsches ergebnis!
