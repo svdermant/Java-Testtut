45c3cb592c1e7589731dd683e7db71ab44e77495
svdermant <svdermant@gmail.com>
2023-09-27 21:36:59 +0200

    Kleines Alterscript... Mit If und Else sowie bestimmung einer Differenz.

--- src/IfThenElse.java
+++ src/IfThenElse.java
@@ -1,14 +1,15 @@
 public class IfThenElse {
     public static void main(String[] args) {
-        int number = 1;
-        boolean erhöhen = (number <= 5); // Anmerkung in einem Boolean kann man also eine Bedingung
-                                         // Für ein IF statement festlegen.
-        if (erhöhen) {
-            number += 3;
-            System.out.println(number);
+        // Kleines Alterscript
+        int ueber18 = 5; // Altersangabe
+        if (ueber18 >= 18) { // Prüft Ob über 18 dann ausgabe
+            System.out.println("Ich bin " + ueber18 + " Jahre alt");
 
-        } else {
-            System.out.println(number);
+        } else { // Wenn jünger als 18
+            int Alter18 = 18;
+            int Diff = Alter18 - ueber18;
+
+            System.out.println("Ich bin " + Diff + " Jahre jünger als 18.");
         }
     }
 }
