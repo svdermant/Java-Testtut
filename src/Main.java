3d11c81376ac879a0b323b1c04bfb78b0be59527
svdermant@gmail.com <svdermant@gmail.com>
2023-09-27 11:43:18 +0200

    Erste Mainklasse Geschrieben.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
 public class Main {
-   /// blablabla
+   public static void main(String[] args) {
+       System.out.println("Hello World");
+       System.out.println("ABS");
+
+       //Variable: Eine Spreichrung für einen Wert
+       //Datentyp: Gibt an, Welche art die Variable ist
+
+       int number = 3;
+       System.out.println(number);
+   }
 }
