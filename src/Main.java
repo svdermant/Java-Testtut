c4a03438d186ab6076ca2104047a9d0290b9bcb9
svdermant@gmail.com <svdermant@gmail.com>
2023-09-27 12:05:45 +0200

    Variablen Deklariert.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,24 @@ public class Main {
        //Variable: Eine Spreichrung für einen Wert
        //Datentyp: Gibt an, Welche art die Variable ist
 
-       int number = 3;
+       int number = 3; // Variable für ganze und negative Zahlen
        System.out.println(number);
+
+       //Andere Variablentype:
+       String text = "Dies ist eine Textvariable"; //Ein String kann Texte Speichern
+       double dezi = 1.37273; //Dieser Typ kann zahlen mit Nachkommastellen Speichern.
+       boolean los = true; //Dieser Type kann nur mit True oder False gesetzt werden und wird in vielen dingen bei Java verwendet.
+
+       // Diese variablen lassen sich jetzt Auch in einer Textausgabe kombinieren da eine Textausgabe immer ein String ist müssen die genannten
+       // Variablen immer mit einem + getrennt werden
+       // Beispiel
+       System.out.println("Hochgerechnet " + dezi + " Menschen sind verstorben diese Therorie ist: " + los);
+       System.out.println(text);
+
+       //Man kann auch einen Rechenaufgabe stellen:
+       int zahl1 = 4;
+       int zahl2 = 10;
+       int sum = zahl1 + zahl2; // Hierbei werden beide Zahlen addiert.
+       System.out.println("Das ergebniss ist: " + sum);
    }
 }
