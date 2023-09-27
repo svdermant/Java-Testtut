2747d884185dd4af3f665a678387bd6739b11f13
svdermant <svdermant@gmail.com>
2023-09-27 23:20:42 +0200

    || Oder Operater besagt mindestens eine Bedinung muss zutreffen.

--- src/Operatoren.java
+++ src/Operatoren.java
@@ -4,17 +4,20 @@ public class Operatoren {
         // ! <- Nicht Operator (Beispiel if (!(temperatur > 20)) Wenn temparatur nicht > 20
         // Bedeutet das gleiche wie (Temparatur <= 20)
         // && <- Und Operator Besagt wenn if bedingung1 und if bedingung2 zutrifft führe code aus.
+        // || <- Oder Operator Besagt das eine beidingung Mindestens erfüllt sein muss um Code auszuführen.
 
-        double temperatur = 25.5; // 45° Sind nicht Angenehm Die folgende If Bedingung liefert also ein
+        double temperatur = 5.5; // 45° Sind nicht Angenehm Die folgende If Bedingung liefert also ein
         // Falsches ergebnis!
         // Lösung wäre eine 2. If anweisung unter der 1. einzubinden die besagt
         // Das Temparatur ebenfalls kleiner als 35 ist....
 
-        // Alte Ifbedinung Übersichtlicher Geschrieben durch einen && Operator
-        if (temperatur > 20 && temperatur < 35) {
-            System.out.println("Angenehm");
-        } else {
+        // Hier sieht man das mindestens 1. Bedingung Zutreffen muss
+        // Geändert wurde die Teparatur auf 5.5
+        // Ausgabe ist also Unangenehm weil 5.5 Grad kleiner als 20 ist.
+        if (temperatur < 20 || temperatur > 35) {
             System.out.println("unangenehm");
+        } else {
+            System.out.println("Angenehm");
         }
     }
 }
