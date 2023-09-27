b5a233f0fe798eab6221fa3961ef6594d57ec2be
svdermant <svdermant@gmail.com>
2023-09-27 20:53:19 +0200

    If Statemant Aktualisiert

--- src/IfThenElse.java
+++ src/IfThenElse.java
@@ -1,11 +1,13 @@
 public class IfThenElse {
     public static void main(String[] args) {
-        int number = 5;
+        int number = 2;
         if (number == 5) { // Bedingung ist immer ein Wert der Wahr oder Falsch ist.
                         // Kriterium ob der Code von dem If block oder Else Block ausgefürt wird.
                         // int number = 5 Ist eine Zuweisung
                         // number == 5 Ist ein Test (Prüfung ob die Zuweisung von Number gleich 5 ist).
                         // Beim Ausführe Gibt die Bedingung ein Wahr zurück.
+                        // Wird die Zuweisung geändert z.b auf 2
+                        // Dann trifft die Bedingung nicht mehr zu und else wird ausgeführt.
             System.out.println("Number ist gleich 5");
 
         } else { // Wird ausgefürt wenn Bedingung Falsch
