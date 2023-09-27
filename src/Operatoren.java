d75c0379f4d5e84058f29514954909cd7f57b499
svdermant <svdermant@gmail.com>
2023-09-27 23:10:35 +0200

    Alte Ifbedinung Übersichtlicher Geschrieben durch einen && Operator

--- src/Operatoren.java
+++ src/Operatoren.java
@@ -3,18 +3,19 @@ public class Operatoren {
         //Logische Operatoren sind Grundlage in Java für Professionalen Code
         // ! <- Nicht Operator (Beispiel if (!(temperatur > 20)) Wenn temparatur nicht > 20
         // Bedeutet das gleiche wie (Temparatur <= 20)
+        // && <- Und Operator Besagt wenn if bedingung1 und if bedingung2 zutrifft führe code aus.
 
         double temperatur = 25.5; // 45° Sind nicht Angenehm Die folgende If Bedingung liefert also ein
-                                  // Falsches ergebnis!
-                                  // Lösung wäre eine 2. If anweisung unter der 1. einzubinden die besagt
-                                  // Das Temparatur ebenfalls kleiner als 35 ist....
+        // Falsches ergebnis!
+        // Lösung wäre eine 2. If anweisung unter der 1. einzubinden die besagt
+        // Das Temparatur ebenfalls kleiner als 35 ist....
 
-        if (temperatur > 20) {
-            if (temperatur < 35) {
-                System.out.println("Angenehm");
-                }
-            } else {
+        // Alte Ifbedinung Übersichtlicher Geschrieben durch einen && Operator
+        if (temperatur > 20 && temperatur < 35) {
+            System.out.println("Angenehm");
+        } else {
             System.out.println("unangenehm");
         }
     }
 }
+
