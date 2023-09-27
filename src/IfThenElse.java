b200f50d8730011f7925c47df07cd973fc943ab0
svdermant <svdermant@gmail.com>
2023-09-28 00:17:52 +0200

    Altersüberprüfung mit Nutzereingabe!

--- src/IfThenElse.java
+++ src/IfThenElse.java
@@ -1,15 +1,17 @@
+import java.util.Scanner;
 public class IfThenElse {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+            Scanner altscan = new Scanner(System.in);
+            int ueber18;
+            System.out.println("Bitte sag mir dein Alter?");
         // Kleines Alterscript
-        int ueber18 = 5; // Altersangabe
+        ueber18 = altscan.nextInt(); // Alterscanner
         if (ueber18 >= 18) { // Prüft Ob über 18 dann ausgabe
-            System.out.println("Ich bin " + ueber18 + " Jahre alt");
+            System.out.println("Du bist " + ueber18 + " Jahre alt");
 
         } else { // Wenn jünger als 18
-            int Alter18 = 18;
-            int Diff = Alter18 - ueber18;
-
-            System.out.println("Ich bin " + Diff + " Jahre jünger als 18.");
+            System.out.println("Du bist jünger als 18 Jahre");
+            System.out.println("Du bist genau: " + ueber18 + " Jahre Alt!");
         }
     }
 }
