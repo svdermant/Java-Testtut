ecd876798f8e47cad78468530e3ceb1cba925a9f
svdermant <svdermant@gmail.com>
2023-09-27 23:41:50 +0200

    Nutzereingaben werden Mittels Java.utils.Scanner erledigt eine Klasse die Vorhandene Funktionen dafür hat.

---
+++ src/NutzerEingabe.java
@@ -0,0 +1,10 @@
+// Für eine Nutzereingabe müssen Daten gespeichert und eingelesen werden
+// hierbei hilft der import des Scanners
+// Dieser Import hat schon Vordefinierte Funktionen die es ermöglichen Nutzereingaben einzulesen etc..
+import java.util.Scanner;
+
+public class NutzerEingabe {
+    public static void main(String[] args) {
+
+    }
+}
