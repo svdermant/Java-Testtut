aba56ac432c93dfb8671e3b1d6effe38997c6221
svdermant <svdermant@gmail.com>
2023-09-28 00:02:02 +0200

    Kommentare für die Zahlenwerte vergessen.

--- src/NutzerEingabe.java
+++ src/NutzerEingabe.java
@@ -7,12 +7,12 @@ public class NutzerEingabe {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); // Ermöglicht das Einlesen von daten.
         String sentense; // Hier wird dann der Eingegebene Satz (String) gespeichert.
-        int zahl;
+        int zahl; // Hier wird eine Zahl als Integer gespeichert
 
         System.out.println("Bitte gib eine Zahl ein!"); //Aufforderung einen Satz einzugeben.
 
         //sentense = scan.nextLine(); //Eingabe wird gespeichert in sentense
-        zahl = scan.nextInt();
+        zahl = scan.nextInt(); //Eingabe wird gespeichert in zahl
 
         System.out.println("Deine Zahl war: " + zahl);
 
